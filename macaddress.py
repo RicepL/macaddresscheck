4837b13ec23b2bcb438db73d2a713099ea46d851
lrice <leon.price.997@gmail.com>
2015-10-11 14:40:28 +0100

    check mac address vendor

---
+++ macaddress.py
@@ -0,0 +1,13 @@
+while 0==0: #loop program
+    venlist = open("vendorlist.txt","r")#open the vendor list downloaded from wire shark(if you dont have it download it by searching mac address vendor click the shark link and then click the listed link 
+    inmacad = input ("what is the mac beggining of the mac address? (00:00:00)") #where the beggining of the address will be input
+    temp=1
+    x=0
+    while x==0:
+        item = venlist.readline()#read the line(not print it)
+        if inmacad in item:#check if it contains the macaddress (if not will then go onto next line)
+            print(item)
+            x=1
+
+    venlist.close()#enable you to try another mac adress after it give you the answer for the first (without it will start from previous line)
+
